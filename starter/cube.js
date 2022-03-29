1face11993742fb6bfc0237d3a331abeb12558ca
Robbie Dorsey <airgo32@gmail.com>
2022-03-28 21:52:05 -0400

    finished

--- starter/cube.js
+++ starter/cube.js
@@ -1,5 +1,12 @@
 "use strict";
-
+/**
+ * Robbie Dorsey
+ * Rubik's Cube Part 2
+ * 
+ * Extra features:
+ * - Cube scrambles when page is first loaded
+ * - Arrow keys rotate the whole cube at once, around either the X or Y axis
+ */
 import { gl, shaderProgram, setup, Transform } from "./cis487.js";
 let transformUniform, colorUniform, overallTransform;
 
@@ -14,9 +21,9 @@ const ORANGE = [0.9, 0.5, 0.0];
 const WHITE  = [0.9, 0.9, 0.9];
 const YELLOW = [1.0, 0.8, 0.0];
 
-// SPEED should be a factor of 90
+// speed should be a factor of 90
 // (or have an integer multiple equal to 90)
-const SPEED = 2
+let speed = 5
 
 setup("cube.vert", "cube.frag").then(main);
 
@@ -86,8 +93,6 @@ class SubCube {
 
 let cv = document.querySelector("#canvas");
 
-
-
 function main() {
   gl.clearColor(1, 1, 1, 1);
   gl.enable(gl.DEPTH_TEST);
@@ -114,8 +119,7 @@ function main() {
   overallTransform = new Transform();
   overallTransform.frustum(1, 0.75, 5, 35);
   overallTransform.translate(0, 0, -20);
-  // overallTransform.translate(0,0,0)
-  overallTransform.rotate(20, "X").rotate(-30, "Y");
+  overallTransform.rotate(25, "X").rotate(-45, "Y");
 
   const subCubes = [ [ [],[],[] ],
                      [ [],[],[] ],
@@ -132,8 +136,9 @@ function main() {
 
   function mousedownHandler(event) {
     clicking = true
-    mouseDownPoint.X = event.pageX - cv.offsetLeft
-    mouseDownPoint.Y = event.pageY - cv.offsetTop
+    speed = 5
+    mouseDownPoint.X = event.pageX
+    mouseDownPoint.Y = event.pageY
   }
   
   function mouseupHandler(event) {
@@ -148,24 +153,92 @@ function main() {
 
       let inverseTransform = Transform.invert(overallTransform)
 
+      const downCoord = getCVVCoords(mouseDownPoint)
       const upCoord = getCVVCoords(mouseUpPoint)
 
+      // get the inverted coordinates for the line through the CVV from the mouse press
+      let downNegZCoord = [downCoord.X, downCoord.Y, -1, 1]
+      let downPosZCoord = [downCoord.X, downCoord.Y, 1, 1]
+      let downNegZTransformed = inverseTransform.transformVector(downNegZCoord)
+      let downPosZTransformed = inverseTransform.transformVector(downPosZCoord)
+
+      // get the inverted coordinates for the line through the CVV from the mouse release
       let upNegZCoord = [upCoord.X, upCoord.Y, -1, 1]
       let upPosZCoord = [upCoord.X, upCoord.Y, 1, 1]
       let upNegZTransformed = inverseTransform.transformVector(upNegZCoord)
       let upPosZTransformed = inverseTransform.transformVector(upPosZCoord)
 
-      let upPlane = determinePlane(upNegZTransformed, upPosZTransformed)
-      getPlaneCoords(upPlane, upPosZTransformed, upNegZTransformed)
-  
-      // new rotation
-      rotateGroup(upPlane, 1, true)
+      // get the plane that was initially clicked
+      let plane = determinePlane(downPosZTransformed, downNegZTransformed)
 
+      // get the coordinates relative to the clicked on plane ( (X, Y), (X, Z) or (Y, Z) )
+      let downPlaneCoords = getPlaneCoords(plane, downPosZTransformed, downNegZTransformed)
+      let upPlaneCoords = getPlaneCoords(plane, upPosZTransformed, upNegZTransformed)
+
+      // axis closest matching the drag, as well as the distance dragged
+      let dir = dragDirection(downPlaneCoords, upPlaneCoords)
+
+      // get the subcube coordinates of the plane clicked (either -1, 0, or 1)
+      let downSubCoords = convertToSubCubeCoords(downPlaneCoords)
+
+      if (dir.distance) { // if the user clicks but doesn't drag, don't do a rotation
+
+        if (plane == "X") {
+          if (dir.direction == "Y") {
+            rotateGroup("Z", downSubCoords.Z, dir.distance > 0)
+
+          } else {
+            rotateGroup("Y", downSubCoords.Y, dir.distance < 0)
+          }
+        }
+
+        if (plane == "Y") {
+          if (dir.direction == "X") {
+            rotateGroup("Z", downSubCoords.Z, dir.distance < 0)
+
+          } else {
+            rotateGroup("X", downSubCoords.X, dir.distance > 0)
+          }
+        }
+
+        if (plane == "Z") {
+          if (dir.direction == "X") {
+            rotateGroup("Y", downSubCoords.Y, dir.distance > 0)
+
+          } else {
+            rotateGroup("X", downSubCoords.X, dir.distance < 0)
+          }
+        }
+      }
+    }
+  }
+
+  function keyDownHandler(event) {
+    speed = 5
+    switch (event.code) {
+      case "ArrowLeft":
+        rotateGroup("Y", 2, false)
+        break;
+
+      case "ArrowRight":
+        rotateGroup("Y", 2, true)
+        break;
+
+      case "ArrowUp":
+        rotateGroup("X", 2, false)
+        break;
+
+      case "ArrowDown":
+        rotateGroup("X", 2, true)
+        break;
     }
   }
   
   cv.addEventListener("mousedown", mousedownHandler);
   window.addEventListener("mouseup", mouseupHandler);
+  window.addEventListener("keydown",  keyDownHandler);
+
+   
 
   function getCVVCoords(rawCoords) {
 
@@ -226,7 +299,7 @@ function main() {
 
   function getPlaneCoords(plane, posCoord, negCoord) {
 
-    let t, xCoord, yCoord, zCoord
+    let t, result = {}
 
     const xEqu = {
       intercept: negCoord[0],
@@ -244,44 +317,91 @@ function main() {
     switch (plane) {
       case "X":
         t = (1.5 - xEqu.intercept) / xEqu.change
-        yCoord = yEqu.change * t + yEqu.intercept
-        zCoord = zEqu.change * t + zEqu.intercept
+        result.Y = yEqu.change * t + yEqu.intercept
+        result.Z = zEqu.change * t + zEqu.intercept
       break;
 
       case "Y":
         t = (1.5 - yEqu.intercept) / yEqu.change
-        xCoord = xEqu.change * t + xEqu.intercept
-        zCoord = zEqu.change * t + zEqu.intercept
+        result.X = xEqu.change * t + xEqu.intercept
+        result.Z = zEqu.change * t + zEqu.intercept
       break;
 
       case "Z":
         t = (1.5 - zEqu.intercept) / zEqu.change
-        xCoord = xEqu.change * t + xEqu.intercept
-        yCoord = yEqu.change * t + yEqu.intercept
+        result.X = xEqu.change * t + xEqu.intercept
+        result.Y = yEqu.change * t + yEqu.intercept
       break;
     }
-
-    // if (xCoord) console.log("X: " + xCoord)
-    // if (yCoord) console.log("Y: " + yCoord)
-    // if (zCoord) console.log("Z: " + zCoord)
-    // console.log("---------------------")
+    return result
   }
 
-  function rotateGroup(axis, group, ccw) {
+  function convertToSubCubeCoords(coords) {
+    let result = {}
+
+    if (coords.X) result.X = Math.floor(coords.X + 0.5)
+    if (coords.Y) result.Y = Math.floor(coords.Y + 0.5)
+    if (coords.Z) result.Z = Math.floor(coords.Z + 0.5)
+
+    return result;
+  }
+
+  function dragDirection(start, end) {
+    let xDistance, yDistance, zDistance
+
+    if (!start.X) { // on the X axis
+      yDistance = end.Y - start.Y
+      zDistance = end.Z - start.Z
+
+      if (Math.abs(yDistance) > Math.abs(zDistance)) {
+        return {direction: "Y", distance: yDistance}
+      } else {
+        return {direction: "Z", distance: zDistance}
+      }
+    }
+
+    if (!start.Y) { // on the Y axis
+      xDistance = end.X - start.X
+      zDistance = end.Z - start.Z
+
+      if (Math.abs(xDistance) > Math.abs(zDistance)) {
+        return {direction: "X", distance: xDistance}
+      } else {
+        return {direction: "Z", distance: zDistance}
+      }
+    }
+
+    if (!start.Z) { // on the Z axis
+      xDistance = end.X - start.X
+      yDistance = end.Y - start.Y
+
+      if (Math.abs(xDistance) > Math.abs(yDistance)) {
+        return {direction: "X", distance: xDistance}
+      } else {
+        return {direction: "Y", distance: yDistance}
+      }
+    }
+  }
+
+  function rotateGroup(axis, group, ccw, count = 1) {
+
     cv.removeEventListener("mousedown", mousedownHandler);
     window.removeEventListener("mouseup", mouseupHandler);
-
+    window.removeEventListener("keydown", keyDownHandler)
 
     let inRotGroup;
 
-    if      (axis == "X") inRotGroup = sc => sc.x == group;
-    else if (axis == "Y") inRotGroup = sc => sc.y == group;
-    else    /* Z */       inRotGroup = sc => sc.z == group;
+    const axes = ["X", "Y", "Z"]
+
+    // I added a second condition for when rotating the entire cube at once
+    if      (axis == "X") inRotGroup = sc => sc.x == group || group == 2;
+    else if (axis == "Y") inRotGroup = sc => sc.y == group || group == 2;
+    else    /* Z */       inRotGroup = sc => sc.z == group || group == 2;
 
     let f = 0;
 
     function animate() {
-      let factor = (-Math.cos(f * SPEED * Math.PI / (45))+1) * SPEED
+      let factor = (-Math.cos(f * speed * Math.PI / (45))+1) * speed
 
       for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
@@ -290,7 +410,7 @@ function main() {
 
             if (inRotGroup(sc)) {
               sc.transform.preRotate(ccw ? 1 * factor : -1 * factor, axis);
-              if ((f + 1) % (90 / SPEED) == 0) sc.rotateXYZ(ccw, axis);
+              if ((f + 1) % (90 / speed) == 0) sc.rotateXYZ(ccw, axis);
             }
 
             sc.draw();
@@ -298,15 +418,28 @@ function main() {
         }
       }
       
-      if ((f + 1) % (90 / SPEED) == 0) {
-        cv.addEventListener("mousedown", mousedownHandler);
-        window.addEventListener("mouseup", mouseupHandler);
+      if ((f + 1) % (90 / speed) == 0) {
+
+        if (count - 1 > 0) { // if there's more scrambles left
+          let randomAxis = axes[Math.floor(Math.random() * 3)]
+          let randomGroup = Math.floor(Math.random() * 3) - 1
+          let randomDirection = Math.floor(Math.random() + 0.5)
+          rotateGroup(randomAxis, randomGroup, randomDirection, count - 1)
+
+        } else {
+          cv.addEventListener("mousedown", mousedownHandler);
+          window.addEventListener("mouseup", mouseupHandler);
+          window.addEventListener("keydown", keyDownHandler)
+        }
+        
       }
-      if (++f < 90 / SPEED) requestAnimationFrame(animate);
+      if (++f < 90 / speed) requestAnimationFrame(animate);
     }
 
     animate();
   }
 
-  // rotateGroup("Y", -1, true);
+    // scrambles the cube
+    speed = 15
+    rotateGroup("Z", 1, false, 45)
 }
