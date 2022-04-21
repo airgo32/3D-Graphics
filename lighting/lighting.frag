fa9f8812b56f81553b597216b9cd75f94a5029a2
Robbie Dorsey <airgo32@gmail.com>
2022-04-20 22:00:07 -0400

    Grass and flickering fire

--- lighting/lighting.frag
+++ lighting/lighting.frag
@@ -1,9 +1,16 @@
 precision mediump float;
 
 const float bands = 8.0;
-const int numLights = 1;
+const int numLights = 2;
+
+const vec3 directionLightVec = normalize(vec3(5.0, 12.0, 5.0));
+// (1.0, 0.4, 0.1)
+// (0.1, 0.1, 0.4)
+const vec3 directionLightCol = (vec3(0.0, 0.4, 1.0));
 
 uniform vec3 uColor;
+
+uniform float lightStrengths[numLights];
 uniform vec3 lightPositions[numLights];
 uniform vec3 lightColors[numLights];
 uniform mat4 lightTransform;
@@ -31,11 +38,13 @@ void main(void) {
         vec3 lightPos = (lightTransform * vec4(lightPositions[i], 1.0)).xyz;
 
         float dist = distance(vPosition, lightPos);
-        dist = 8.0 / dist;
+        dist = lightStrengths[i] / dist;
         dist = max(2.0, dist) - 2.0;
 
         vec3 lightDirection = lightPos - vPosition;
-        float dp = max(0.0, dot(normalize(lightDirection), normalize(vNormal)));
+        // float dp = max(0.0, dot(normalize(lightDirection), normalize(vNormal)));
+        float dp = abs(dot(normalize(lightDirection), normalize(vNormal)));
+
 
         vec3 finalColor = max(vec3(0.0, 0.0, 0.0), normalize(lightColors[i]) - complement(uColor));
         finalColor *= dp * dist * 0.6;
@@ -46,8 +55,20 @@ void main(void) {
         // brightness = maxLight(brightness, final);
         brightness += finalColor;
     }
+
+    // directional light
+    bool directionOn = true;
+    if (directionOn) {
+        float dp = max(0.0, dot(directionLightVec, normalize(vNormal)));
+        vec3 finalColor = max(vec3(0.0, 0.0, 0.0), (directionLightCol) - (complement(uColor)));
+        finalColor *= dp * 2.0;
+        brightness += finalColor;
+
+
+    }
+
     // brightness += uColor;
-    vec3 ambient = uColor * 0.2;
+    vec3 ambient = uColor * 0.1;
     vec3 color = brightness + ambient;
 
     gl_FragColor = vec4(color, 1.0);
--- lighting/lighting.js
+++ lighting/lighting.js
@@ -7,63 +7,29 @@ setup("lighting.vert", "lighting.frag").then(main);
 
 function main() {
     let mvTransform, pTransform, nTransform, lightTransform
-    const lightPositions = [
-        4.0, 0.0, 0.0,
-        0.0, 0.0, 4.0,
+    let lightPositions = [
+        0.0, 0.0, 0.0,
+        // 0.0, 4.0, 0.0,
     ]
-    const lightColors = [
-        1.0, 1.0, 1.0,
-        0.0, 1.0, 1.0,
+    let lightColors = [
+        0.9, 0.2, 0.0,
+        // 1.0, 1.0, 1.0,
+    ]
+    let lightStrengths = [
+        25,
+    ]
 
-    ]
     let mvHistory = []
 
-    const BLUE = [0.1, 0.4, 0.8];
+    // const BLUE = [0.1, 0.4, 0.8];
+    const BLUE = [0.0, 0.0, 1.0];
+
     const RED = [0.9, 0.1, 0.1];
     const GREEN = [0.1, 0.8, 0.2];
     const WHITE = [1.0, 1.0, 1.0];
     const BLACK = [0.0, 0.0, 0.0];
 
-
-
-    function drawCube() {
-
-        function drawSquare() {
-
-            mvHistory.push(mat4.clone(mvTransform));
-            // magic goes here
-            mat4.translate(mvTransform, mvTransform, [0, 0, 0.5]);
-
-            mat4.invert(nTransform, mvTransform)
-            mat4.transpose(nTransform, nTransform)
-
-            gl.uniform3f(uniforms.uColor, ...RED);
-            gl.uniformMatrix4fv(uniforms.mvTransform, false, mvTransform);
-            gl.uniformMatrix4fv(uniforms.nTransform, false, nTransform);
-            gl.uniformMatrix4fv(uniforms.lightTransform, false, lightTransform);
-            gl.drawArrays(gl.TRIANGLE_FAN, 0, 4)
-
-            mvTransform = mvHistory.pop()
-
-        }
-
-        mvHistory.push(mat4.clone(mvTransform));
-
-        drawSquare()
-        mat4.rotateY(mvTransform, mvTransform, Math.PI / 2)
-        drawSquare()
-        mat4.rotateY(mvTransform, mvTransform, Math.PI / 2)
-        drawSquare()
-        mat4.rotateY(mvTransform, mvTransform, Math.PI / 2)
-        drawSquare()
-        mat4.rotateX(mvTransform, mvTransform, Math.PI / 2)
-        drawSquare()
-        mat4.rotateX(mvTransform, mvTransform, Math.PI)
-        drawSquare()
-
-        mvTransform = mvHistory.pop()
-
-    }
+    const CLEAR_COLOR = BLACK;
 
     function drawFromTris(color) {
         mvHistory.push(mat4.clone(mvTransform));
@@ -80,14 +46,59 @@ function main() {
         // CHANGE TO 4V LATER
         gl.uniform3fv(uniforms.lightPositions, lightPositions);
         gl.uniform3fv(uniforms.lightColors, lightColors);
+        gl.uniform1fv(uniforms.lightStrengths, lightStrengths);
 
         gl.drawArrays(gl.TRIANGLES, 0, columnVertexData.length / 3)
 
         mvTransform = mvHistory.pop()
     }
 
+    function generateGrass(spawnX, spawnZ) {
+        let x1, z1, x2, z2, x3, y3, z3, r, g, b
 
-    gl.clearColor(0, 0, 0, 1);
+        x1 = spawnX + (Math.random() * 0.3) - 0.15
+        z1 = spawnZ + (Math.random() * 0.3) - 0.15
+
+        x2 = spawnX + (Math.random() * 0.3) - 0.15
+        z2 = spawnZ + (Math.random() * 0.3) - 0.15
+
+        x3 = spawnX + ((x1 - x2) / 2) * Math.random() * 0.5
+        y3 = 0.6 + (Math.random() * 0.3)
+        z3 = spawnZ + ((z1 - z2) / 2) * Math.random() * 0.5
+
+        r = (Math.random() * 0.2)
+        g = 0.2 + (Math.random() * 0.1)
+        b = 0.0 + (Math.random() * 0.1)
+
+        let v1 = vec3.fromValues(x2 - x1, 0.0, z2 - z1)
+        let v2 = vec3.fromValues(x3 - x2, y3, z3 - z2)
+
+        let cross = vec3.create()
+        vec3.cross(cross, v1, v2)
+        vec3.normalize(cross, cross)
+
+        return {
+            triangle: [
+                x1, 0.0, z1,
+                x2, 0.0, z2,
+                x3, y3,  z3,
+            ],
+            normals: [
+                cross[0], cross[1], cross[2],
+                cross[0], cross[1], cross[2],
+                cross[0], cross[1], cross[2],
+            ],
+            color: [
+                r, g, b,
+                r, g, b,
+                r, g, b,
+            ],
+            rotationOffset: Math.floor(Math.random() * 4)
+
+        }
+    }
+
+    gl.clearColor(...CLEAR_COLOR, 1);
     gl.enable(gl.DEPTH_TEST);
 
     const uniforms = {
@@ -99,6 +110,7 @@ function main() {
 
         lightPositions: gl.getUniformLocation(shaderProgram, "lightPositions"),
         lightColors: gl.getUniformLocation(shaderProgram, "lightColors"),
+        lightStrengths: gl.getUniformLocation(shaderProgram, "lightStrengths"),
     }
 
     const attributes = {
@@ -335,15 +347,31 @@ function main() {
         0.0, -1.0, 0.0,
     ]
 
+    let grassVertexData = [
+
+    ]
+
+    let grassColors = [
+
+    ]
+
+    let grassNormalData = [
+
+    ]
+
+    let grassRotationOffsets = [
+        
+    ]
+
     let vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(columnVertexData), gl.STATIC_DRAW)
+    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(columnVertexData), gl.STATIC_DRAW)
     gl.vertexAttribPointer(attributes.position, 3, gl.FLOAT, false, 0, 0)
 
     // put a normal buffer here when we're ready for it
     let normalBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(columnNormalData), gl.STATIC_DRAW)
+    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(columnNormalData), gl.STATIC_DRAW)
     gl.vertexAttribPointer(attributes.normalVector, 3, gl.FLOAT, false, 0, 0)
 
     // transforms go here
@@ -359,49 +387,78 @@ function main() {
     mat4.scale(mvTransform, mvTransform, [0.5, 0.5, 0.5])
     mat4.rotateX(mvTransform, mvTransform, Math.PI * 20 / 180)
     mat4.rotateY(mvTransform, mvTransform, Math.PI * -30 / 180)
-
-    // mat4.rotateX(mvTransform, mvTransform, Math.PI/2)
+    mat4.translate(mvTransform, mvTransform, [0, -2, 0])
 
     lightTransform = mat4.clone(mvTransform)
 
-    // mat4.rotateX(mvTransform, mvTransform, Math.PI * 30 / 180 )
+    for (let i = 0; i < 25; i++) {
+        let blade = generateGrass((4 * Math.random()) - 2, (4 * Math.random()) - 2)
 
+        grassVertexData.push(...blade.triangle)
+        grassColors.push(...blade.color)
+        grassNormalData.push(...blade.normals)
+        grassRotationOffsets.push(blade.rotationOffset)
+    }
 
-    // let v1 = vec3.fromValues(0, 5, 0)
-    // let v2 = vec3.fromValues((Math.SQRT2 / 2) - 1, 0, (Math.SQRT2 / 2))
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(grassVertexData), gl.STATIC_DRAW)
 
-    // let v = vec3.create();
-    // vec3.cross(v, v1, v2)
-    // vec3.normalize(v, v)
-    // console.log(v)
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(grassNormalData), gl.STATIC_DRAW)
+
     let frameCount = 0;
     let turnRate = 1;
+
 
     function animate() {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 
-        if (frameCount == 0) {
-            mat4.rotateY(lightTransform, lightTransform, Math.PI * turnRate / 90)
+        if (frameCount % turnRate == 0) {
+            // mat4.rotateY(lightTransform, lightTransform, Math.PI * turnRate / 90)
             // mat4.rotateY(mvTransform, mvTransform, Math.PI * turnRate / 900);
+            // mat4.translate(lightTransform, lightTransform, [0, Math.sin(Math.PI * frameCount / 180), 0])
 
         }
+        // mat4.translate(lightTransform, lightTransform, [0, Math.sin(frameCount / 28) / 45, 0])
+        // mat4.translate(mvTransform, mvTransform, [0, Math.sin(frameCount / 28) / 28, 0])
+
         // mat4.rotateZ(mvTransform, mvTransform, Math.PI / 900)
         // mat4.rotateX(mvTransform, mvTransform, Math.PI / 900)
 
-        // drawCube()
         mvHistory.push(mat4.clone(mvTransform))
-        drawFromTris(WHITE)
-        mat4.translate(mvTransform, mvTransform, [-8, 0, 0])
-        drawFromTris(RED)
-        mat4.translate(mvTransform, mvTransform, [8, 0, -8])
-        mat4.rotateY(mvTransform, mvTransform, Math.PI / 8)
-        drawFromTris(BLUE)
+        let plotSize = 5;
+
+        mat4.translate(mvTransform, mvTransform, [-2 * (plotSize - 1), 0, -2 * (plotSize - 1)])
+        for (let i = 0; i < plotSize; i++) {
+            for (let j = 0; j < plotSize; j++) {
+                if (!(
+                    i == 0 & j == 0 ||
+                    i == 0 & j == 4 ||
+                    i == 4 & j == 0 ||
+                    i == 4 & j == 4
+                )) {
+                    mat4.rotateY(mvTransform, mvTransform,Math.PI / 2 * grassRotationOffsets[(i * 5) + j])
+                    drawFromTris(GREEN)
+                    mat4.rotateY(mvTransform, mvTransform,Math.PI / -2 * grassRotationOffsets[(i * 5) + j])
+                }
+                
+                mat4.translate(mvTransform, mvTransform, [4, 0, 0])
+            }
+            mat4.translate(mvTransform, mvTransform, [-4 * plotSize, 0, 4])
+        }
+
+        let funcA = (Math.sin(frameCount * Math.PI / 180) + 1)
+        let funcB = (Math.sin(2 * frameCount * Math.PI / 180) + 1)
+        let funcC = (Math.sin(4 * frameCount * Math.PI / 180) + 1)
+        lightStrengths[0] = 10 + funcB + (Math.cos(frameCount * Math.PI / 18) * funcA * funcB * funcC)
+
 
         mvTransform = mvHistory.pop()
 
+    
         frameCount++;
-        frameCount %= turnRate;
+        // frameCount %= turnRate;
         requestAnimationFrame(animate)
     }
 
